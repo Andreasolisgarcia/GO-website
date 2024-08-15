66b9a78e65b16efc061ca781099aeb4e511d3f7a
Andreasolisgarcia <andrea.solisg@gmail.com>
2024-08-15 15:37:24 +0200

    adjusting modal js

--- js/modal.js
+++ js/modal.js
@@ -6,22 +6,15 @@ const formData = document.querySelectorAll(".formData");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+function launchModal() {
+  modalbg.classList.add("bground-open");
+}
+
 // close Fonction event
 
-function addCloseEventToBtns() {
-  const closeBtns = document.querySelectorAll(".close");
-  closeBtns.forEach((btn) => btn.addEventListener("click", closeModal));
-}
+const closeBtns = document.querySelectorAll(".close");
+closeBtns.forEach((btn) => btn.addEventListener("click", closeModal));
 
-addCloseEventToBtns() 
-
-// launch modal form
-function launchModal() {
-  modalbg.classList.add('bground-open')
-
-}
-
-// close modal Function
 function closeModal() {
-  modalbg.classList.remove('bground-open');
+  modalbg.classList.remove("bground-open");
 }
